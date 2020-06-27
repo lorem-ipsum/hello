b6ebdab1faa6ba17a8018d06f83c5483f00cf7aa
clt <lo-ipsum@protonmail.com>
2020-06-27 17:43:51 +0800

    add pages

--- hello/urls.py
+++ hello/urls.py
@@ -13,9 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import pages.views
 from django.contrib import admin
 from django.urls import path
 
+admin.autodiscover()
+
+
 urlpatterns = [
+    path('/', pages.views.index, name="index"),
     path('admin/', admin.site.urls),
 ]
